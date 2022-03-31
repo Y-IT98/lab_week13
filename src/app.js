f4654022ce1b1609ff84ad5db1d519eb5d4873f5
youssef <youssef_nassar98@outlook.com>
2022-03-31 15:40:34 -0700

    Added code to my javascript file to add functionality to the clear the copies from container div.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
-const Div_event = document.querySelectorAll('.original-container div')
-const Area_copied = document.querySelector ('.copy-container')
+const Div_event = document.querySelectorAll(".original-container div");
+const Area_copied = document.querySelector(".copy-container");
 
+//*// Clone
+Div_event.forEach((element) =>
+  element.addEventListener("click", (evt) => {
+    let cloneMaterial = evt.target.parentElement.outerHTML;
+    Area_copied.insertAdjacentHTML("beforeend", cloneMaterial);
+  })
+);
 
-Div_event.forEach(element => element.addEventListener('click',(evt) => {
-    let material_cloned = evt.target.parentElement.outerHTML ;
-    Area_copied.insertAdjacentHTML ("beforeend", cloneMaterial);
-}))
+/* Delete button*/
+const button_reset = document.querySelector("button");
 
+button_reset.addEventListener("click", () => {
+  Area_copied.innerHTML = "";
+});
